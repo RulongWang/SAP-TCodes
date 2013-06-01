65a688d8fdb5a4e04dd8c5947c29c189f7df4934
rulongwang <rulong.wang@gmail.com>
2013-06-01 13:30:10 +0800

    Initial Commit

---
+++ SAP TCodes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SAP TCodes
+//
+//  Created by rulong on 6/1/13.
+//  Copyright (c) 2013 rulong.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RLAppDelegate class]));
+    }
+}
